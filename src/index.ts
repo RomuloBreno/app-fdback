8a69b760385cef5896a3ffb77739d134c477fb3e
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-10-30 05:14:50 -0300

    new api

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-import { Student } from './entities/Student.ts';
-import { Product } from './entities/Product.ts';
+import http from 'http';
 
-import { StudentFactory } from './factories/StudentFactory.ts';
-import { ProductFactory } from './factories/ProductFactory.ts';
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('to work');
+});
 
-const studentService = StudentFactory.getInstance();
-const productService = ProductFactory.getInstance();
-
-const erick = new Student({ id: 1, name: 'Erick' })
-const ana = new Student({ id: 2, name: 'Ana' })
-
-const laptop = new Product({ id: 1, title: 'Laptop', price: 999.99 });
-
-studentService.create(erick);
-studentService.create(ana);
-
-studentService.update(ana.id, new Student({ id: 2, name: 'Ana Silva' }));
-studentService.delete(erick.id);
-
-console.log("Students:");
-for (const student of studentService.read()) {
-    console.log(`ID: ${student.id}, Name: ${student.name}`);
-}
-
-productService.create(laptop);
-console.log("Products:");
-for (const product of productService.read()) {
-    console.log(`ID: ${product.id}, Title: ${product.title}, Price: ${product.price}`);
-}
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
