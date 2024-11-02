208cf84106c2118140f4e719bc49aa0cfb311cee
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-11-02 12:14:47 -0300

    add middleware to lçimit requests

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -3,7 +3,7 @@ import { verifyToken } from '../utils/tokenUtil.ts';
 export const authMiddleware = (req: any, res: any, next: any) => {
   try {
     const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(401).json({ result: 'Token não fornecido' });
+    if (!token) return res.status(404).json({ result: 'Token não fornecido' });
     const decoded = verifyToken(token);
     req.userId = decoded.userId;
     next();
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import AuthController from '../controller/AuthController.ts';
 import { authMiddleware } from '../middleware/authMiddleware.ts';
+import { rateLimiter } from '../middleware/RateLimit.ts';
 
 const authRouter = Router();
 
-authRouter.post('/register',AuthController.register);
-authRouter.post('/login', AuthController.login);
-authRouter.use('/protected-route', authMiddleware, (req:any, res:any) => {
+authRouter.post('/register', rateLimiter, AuthController.register);
+authRouter.post('/l-fdback', rateLimiter,  AuthController.login);
+authRouter.use('/protected-route', rateLimiter,  authMiddleware, (req:any, res:any) => {
     res.json({ message: `Acesso autorizado! ID do usuário: ${req.userId}` });
   });
 
