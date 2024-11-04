4f90d4a1568bce97c7f099d2471a734ba8abfd3f
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-11-04 09:33:57 -0300

    fix route t-fdback

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import AuthController from '../controller/AuthController.ts';
-import { authMiddleware } from '../middleware/AuthMiddleware.ts'
 import { rateLimiter } from '../middleware/RateLimit.ts';
-import { verifyToken } from '../utils/tokenUtil.ts';
+
 
 const authRouter = Router();
 
 authRouter.post('/register', rateLimiter, AuthController.register);
 authRouter.post('/l-fdback', rateLimiter,  AuthController.login);
-authRouter.get('/t-fdback', AuthController.decrypt);
+authRouter.get('/t-fdback', rateLimiter,AuthController.decrypt);
 
 export {authRouter};
