32a967d8ed897a2856a45dde66086f608929a40c
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-11-04 09:29:21 -0300

    add validantion uri

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export async function connect() {
-    await mongoose.connect(process.env.MONGO_URI);
+    let mongoUri = process.env.MONGO_URI
+if (!mongoUri) {
+    console.error("A URI do MongoDB não foi definida.");
+    process.exit(1); // Encerra o processo, pois a conexão não será possível
+  }
+    await mongoose.connect(mongoUri);
     console.log('Connected to MongoDB successfully!');
 }
