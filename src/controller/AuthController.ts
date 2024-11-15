40e21a8b0b7353c56f03282c55e59f1a0d444cc1
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-11-15 09:30:31 -0300

    test

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -2,6 +2,7 @@ import AuthService from '../services/AuthService.ts'
 import { verifyToken } from '../utils/tokenUtil.ts';
 import { isBase64 } from '../utils/base64Utils.ts';
 import path from 'path';
+
 class AuthController {
   async register(req: any, res: any): Promise<any> {
     //validação para envio de json
